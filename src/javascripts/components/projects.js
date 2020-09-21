4bd396c387c91fef485957f840b1628cae3fc7a9
Sam Mudick <smudick@gmail.com>
2020-09-20 23:56:04 -0500

    updated projects and restyled

--- src/javascripts/components/projects.js
+++ src/javascripts/components/projects.js
@@ -1,26 +1,25 @@
 import projectData from '../helpers/data/projectData';
 
-const printToDom = (divId, textToPrint) => {
-  const selectedDiv = document.getElementById(divId);
-
-  selectedDiv.innerHTML = textToPrint;
+const builProjectHeader = () => {
+  $('#projectsPage').html(
+    '<div class="projectsHeader"><h2>Projects</h2></div>'
+  );
 };
 
 const buildProjectCards = () => {
-  let domString = '<div class="projectsHeader"><h2>Projects</h2></div>';
-
-  for (let i = 0; i < projectData.getProjects().length; i += 1) {
-    if (projectData.getProjects()[i].available === true) {
-      domString += '<div class"project">';
-      domString += `<div class="projTitle"><h3>${projectData.getProjects()[i].title}</h3></div>`;
-      domString += `<div class="projImage"><img src=${projectData.getProjects()[i].screenshot} alt="Screenshot of project ${projectData.getProjects()[i].title}"></div>`;
-      domString += `<div class="projDescription"><p>${projectData.getProjects()[i].description}</p></div>`;
-      domString += `<div class="projUrl"><a href=${projectData.getProjects()[i].url}>Project Link</a></div>`;
-      domString += `<div class="projGitUrl"><a href=${projectData.getProjects()[i].githubUrl}>Github Link</a></div>`;
-      domString += '</h2>';
-    }
-  }
-  printToDom('projectsPage', domString);
+  builProjectHeader();
+  projectData.getProjects().forEach((item) => {
+    $('#projectsPage').append(`
+  <div class"project">
+  <div class="projTitle"><h3>${item.title}</h3></div>
+  <div class="projImage"><img src=${item.screenshot} alt="Screenshot of project ${item.title}"></div>
+  <div class="projDescription"><p>${item.description}</p></div>
+  <div class="urls">
+  <div class="url"><a href=${item.url}>Project Link</a></div>
+  <div class="url"><a href=${item.githubUrl}>Github Link</a></div>
+  </div>
+  </div>`);
+  });
 };
 
 export default { buildProjectCards };
